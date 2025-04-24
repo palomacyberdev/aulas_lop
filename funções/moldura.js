0e78e4e52d40395f8384c4f5043363bb121db4ef
PALOMA DE SOUZA VICENTE <25170405@educ123.sp.senai.br>
2025-04-24 11:24:55 -0300

    Add file moldura

--- funções/moldura.js
+++ funções/moldura.js
@@ -1,1 +1,21 @@
 // Desenhe uma moldura em uma frase
+const readline = require('readline-sync');
+const caracter='█'
+
+function barra(quantidade){
+    for(let i=0;i<quantidade+4;i++){
+        process.stdout.write(caracter)
+        
+    }
+    console.log();
+}
+//Fora da função
+//Tudo oque está dentro da função é ajustado somente dentro da função
+
+const texto = readline.question("\nDigite uma frase\t");
+
+barra(texto.length);
+barra(texto.length);
+console.log(`${caracter} ${texto} ${caracter}`);
+barra(texto.length);
+barra(texto.length)
